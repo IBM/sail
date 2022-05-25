8ca9c4aee06bb651ad627302d32d093072262b56
Kasper Hjort Berthelsen <khb@2bm.com>
2022-05-25 17:01:51 +0200

    add sigmoid to prevent nan

--- sail/models/torch/tcn.py
+++ sail/models/torch/tcn.py
@@ -7,7 +7,7 @@ from torch import nn
 from torch.nn.utils.weight_norm import weight_norm
 
 
-class GAP1d(nn.Module):
+class _GAP1d(nn.Module):
     "Global Adaptive Pooling + Flatten"
 
     def __init__(self, output_size: int = 1) -> None:
@@ -19,28 +19,40 @@ class GAP1d(nn.Module):
         return self.flatten(self.gap(x))
 
 
-class Chomp1d(nn.Module):
+class _Chomp1d(nn.Module):
     def __init__(self, chomp_size: int) -> None:
-        super(Chomp1d, self).__init__()
+        super(_Chomp1d, self).__init__()
         self.chomp_size = chomp_size
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x[:, :, : -self.chomp_size].contiguous()
 
 
-class TemporalBlock(nn.Module):
+class _TemporalBlock(nn.Module):
+
     def __init__(self, ni, nf, ks, stride, dilation, padding, dropout=0.0):
+        """Initialises the Temporal Block
+
+        Args:
+            ni (int): number of inputs
+            nf (int): number of outputs
+            ks (int): kernel size
+            stride (int): stride lengths
+            dilation (int): dilation
+            padding (int or str): padding
+            dropout (float, optional): dropout rate. Defaults to 0.0.
+        """
         super().__init__()
         self.conv1 = weight_norm(
             nn.Conv1d(ni, nf, ks, stride=stride, padding=padding, dilation=dilation)
         )
-        self.chomp1 = Chomp1d(padding)
+        self.chomp1 = _Chomp1d(padding)
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
         self.conv2 = weight_norm(
             nn.Conv1d(nf, nf, ks, stride=stride, padding=padding, dilation=dilation)
         )
-        self.chomp2 = Chomp1d(padding)
+        self.chomp2 = _Chomp1d(padding)
         self.relu2 = nn.ReLU()
         self.dropout2 = nn.Dropout(dropout)
         self.net = nn.Sequential(
@@ -76,7 +88,7 @@ def TemporalConvNet(c_in, layers, ks=2, dropout=0.0):
         ni = c_in if i == 0 else layers[i - 1]
         nf = layers[i]
         temp_layers += [
-            TemporalBlock(
+            _TemporalBlock(
                 ni,
                 nf,
                 ks,
@@ -88,17 +100,35 @@ def TemporalConvNet(c_in, layers, ks=2, dropout=0.0):
         ]
     return nn.Sequential(*temp_layers)
 
+class _TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(_TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [_TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
 
-class TCN(nn.Module):
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+class _TCN(nn.Module):
     def __init__(
         self, c_in, c_out, layers=8 * [25], ks=7, conv_dropout=0.0, fc_dropout=0.0
     ):
         super().__init__()
-        self.tcn = TemporalConvNet(c_in, layers, ks=ks, dropout=conv_dropout)
-        self.gap = GAP1d()
+        self.tcn = _TemporalConvNet(c_in, layers, kernel_size=ks, dropout=conv_dropout)
+        self.gap = _GAP1d()
         self.dropout = nn.Dropout(fc_dropout) if fc_dropout else None
         self.linear = nn.Linear(layers[-1], c_out)
         self.init_weights()
+        self.sig = nn.Sigmoid()
+
 
     def init_weights(self):
         self.linear.weight.data.normal_(0, 0.01)
@@ -108,7 +138,9 @@ class TCN(nn.Module):
         x = self.gap(x)
         if self.dropout is not None:
             x = self.dropout(x)
-        return self.linear(x)
+        return self.sig(self.linear(x))
+
+
 
 
 class TCNRegressor(NeuralNetRegressor):
@@ -124,7 +156,7 @@ class TCNRegressor(NeuralNetRegressor):
 
     def __init__(self, c_in: int, c_out: int, **kwargs):
         super(TCNRegressor, self).__init__(
-            module=TCN,
+            module=_TCN,
             module__c_in=c_in,
             module__c_out=c_out,
             train_split=None,

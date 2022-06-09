001ce7798bc20a1b80263a9810646ad8861cc6c3
Mads Ehrhorn <ehrhorn@me.com>
2022-06-09 16:23:46 +0200

    Added documentation for TCNModel.

--- sail/models/torch/tcn.py
+++ sail/models/torch/tcn.py
@@ -12,49 +12,26 @@ class _ResidualBlock(nn.Module):
         num_filters: int,
         kernel_size: int,
         dilation_base: int,
-        dropout_fn,
+        dropout_fn: nn.Dropout,
         weight_norm: bool,
         nr_blocks_below: int,
         num_layers: int,
         input_size: int,
         target_size: int,
     ):
-        """PyTorch module implementing a residual block module used in `_TCNModule`.
+        """PyTorch module implementing a residual block module used in `TCNModel`.
+        This implementation is based on https://github.com/unit8co/darts/blob/master/darts/models/forecasting/tcn_model.py
 
-        Parameters
-        ----------
-        num_filters
-            The number of filters in a convolutional layer of the TCN.
-        kernel_size
-            The size of every kernel in a convolutional layer.
-        dilation_base
-            The base of the exponent that will determine the dilation on every level.
-        dropout_fn
-            The dropout function to be applied to every convolutional layer.
-        weight_norm
-            Boolean value indicating whether to use weight normalization.
-        nr_blocks_below
-            The number of residual blocks before the current one.
-        num_layers
-            The number of convolutional layers.
-        input_size
-            The dimensionality of the input time series of the whole network.
-        target_size
-            The dimensionality of the output time series of the whole network.
-
-        Inputs
-        ------
-        x of shape `(batch_size, in_dimension, input_chunk_length)`
-            Tensor containing the features of the input sequence.
-            in_dimension is equal to `input_size` if this is the first residual block,
-            in all other cases it is equal to `num_filters`.
-
-        Outputs
-        -------
-        y of shape `(batch_size, out_dimension, input_chunk_length)`
-            Tensor containing the output sequence of the residual block.
-            out_dimension is equal to `output_size` if this is the last residual block,
-            in all other cases it is equal to `num_filters`.
+        Args:
+            num_filters (int): The number of filters in a convolutional layer of the TCN.
+        kernel_size (int): The size of every kernel in a convolutional layer.
+        dilation_base (int): The base of the exponent that will determine the dilation on every level.
+        dropout_fn (nn.Dropout): The dropout function to be applied to every convolutional layer.
+        weight_norm (bool): Boolean value indicating whether to use weight normalization.
+        nr_blocks_below (int): The number of residual blocks before the current one.
+        num_layers (int): The number of convolutional layers.
+        input_size (int): The dimensionality of the input time series of the whole network.
+        target_size (int): The dimensionality of the output time series of the whole network.
         """
         super().__init__()
 
@@ -85,6 +62,15 @@ class _ResidualBlock(nn.Module):
             self.conv3 = nn.Conv1d(input_dim, output_dim, 1)
 
     def forward(self, x):
+        """_ResidualBlock forward pass.
+
+        Args:
+            x (torch.Tensor): The input data
+
+        Shape:
+            - Input: `(batch_size, in_dimension, input_chunk_length)`
+            - Output: `(batch_size, out_dimension, input_chunk_length)`
+        """
         residual = x
 
         left_padding = (self.dilation_base ** self.nr_blocks_below) * (
@@ -118,42 +104,18 @@ class TCNModel(nn.Module):
         weight_norm: bool,
         dropout: float,
     ):
-
         """PyTorch module implementing a dilated TCN module used in `TCNModel`.
+        This implementation is based on https://github.com/unit8co/darts/blob/master/darts/models/forecasting/tcn_model.py
 
-
-        Parameters
-        ----------
-        input_size
-            The dimensionality of the input time series.
-        target_size
-            The dimensionality of the output time series.
-        kernel_size
-            The size of every kernel in a convolutional layer.
-        num_filters
-            The number of filters in a convolutional layer of the TCN.
-        num_layers
-            The number of convolutional layers.
-        weight_norm
-            Boolean value indicating whether to use weight normalization.
-        dilation_base
-            The base of the exponent that will determine the dilation on every level.
-        dropout
-            The dropout rate for every convolutional layer.
-        **kwargs
-            all parameters required for :class:`darts.model.forecasting_models.PLForecastingModule` base class.
-
-        Inputs
-        ------
-        x of shape `(batch_size, input_chunk_length, input_size)`
-            Tensor containing the features of the input sequence.
-
-        Outputs
-        -------
-        y of shape `(batch_size, input_chunk_length, target_size)`
-            Tensor containing the predictions of the next 'output_chunk_length' points in the last
-            'output_chunk_length' entries of the tensor. The entries before contain the data points
-            leading up to the first prediction, all in chronological order.
+        Args:
+        input_dim (int): The dimensionality of the input time series.
+        output_dim (int): The dimensionality of the output time series.
+        kernel_size (int): The size of every kernel in a convolutional layer.
+        num_filters (int): The number of filters in a convolutional layer of the TCN.
+        num_layers (int): The number of convolutional layers.
+        dilation_base (int): The base of the exponent that will determine the dilation on every level.
+        weight_norm (bool): Boolean value indicating whether to use weight normalization.
+        dropout (float): The dropout rate for every convolutional layer.
         """
 
         super().__init__()
@@ -184,11 +146,19 @@ class TCNModel(nn.Module):
             self.res_blocks_list.append(res_block)
         self.res_blocks = nn.ModuleList(self.res_blocks_list)
 
-    def forward(self, x_in):
-        x = x_in
+    def forward(self, x):
+        """TCNModel forward pass.
+
+        Args:
+            x (torch.Tensor): The input data
+
+        Shape:
+            - Input: `(batch_size, in_dimension, input_chunk_length)`
+            - Output: `(batch_size, out_dimension, input_chunk_length)`
+        """
+        # To aid inputs that omit chunk length, we add a dummy dimension if ndim = 2
         if x.dim() == 2:
             x = torch.unsqueeze(x, dim=-1)
-        # data is of size (batch_size, input_size, input_chunk_length)
         batch_size = x.size(0)
 
         for res_block in self.res_blocks_list:
@@ -200,14 +170,17 @@ class TCNModel(nn.Module):
 
 
 class TCNRegressor(NeuralNetRegressor):
-    """Basic TCN model.
+    """Basic NeuralNetRegressor wrapper for TCNModel.
+
     Args:
-        c_in (int): number of input channels
-        c_out (int): number of output channels
-        layers (list): number of channels in each TCN layer
-        ks (int): kernel size
-        conv_dropout (float): dropout rate for TCN convolutional layers
-        fc_dropout (float): dropout rate for FC layers
+        input_dim (int): The dimensionality of the input time series.
+        output_dim (int): The dimensionality of the output time series.
+        kernel_size (int): The size of every kernel in a convolutional layer.
+        num_filters (int): The number of filters in a convolutional layer of the TCN.
+        num_layers (int): The number of convolutional layers.
+        dilation_base (int): The base of the exponent that will determine the dilation on every level.
+        weight_norm (bool): Boolean value indicating whether to use weight normalization.
+        dropout (float): The dropout rate for every convolutional layer.
         **kwargs: Arbitrary keyword arguments
     """
 

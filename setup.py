df8445c26d85933d371ef641a839c2a8580b4e84
Dhaval Salwala <Dhaval.Vinodbhai.Salwala@ibm.com>
2023-02-12 13:33:00 +0000

    setup.py info updated.
    Signed-off-by: Dhaval Salwala <Dhaval.Vinodbhai.Salwala@ibm.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages, setup
+import sail
 
 if __name__ == "__main__":
     setup(
         name="sail",
-        version="0.2.0",
+        version=sail.__version__,
         description="Python package for streaming data and incremental learning",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
         url="https://github.com/IBM/sail",
         author="Seshu Tirupathi, Dhaval Salwala, Shivani Tomar",
         author_email="seshutir@ie.ibm.com, dhaval.vinodbhai.salwala@ibm.com",
@@ -12,4 +15,12 @@ if __name__ == "__main__":
         python_requires="==3.10.*",
         packages=find_packages(),
         zip_safe=False,
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.10",
+        ],
     )

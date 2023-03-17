392bd6d229628e56afd51e90a8f6869ff81eb2b6
Dhaval Salwala <Dhaval.Vinodbhai.Salwala@ibm.com>
2023-03-17 23:16:23 +0000

    dependencies version update
    Signed-off-by: Dhaval Salwala <Dhaval.Vinodbhai.Salwala@ibm.com>

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SAIL
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) ![main branch](https://github.com/IBM/sail/actions/workflows/build.yml/badge.svg?branch=main)
+<img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a> [![Python](https://img.shields.io/pypi/pyversions/tensorflow.svg)](#)
 
 The library is for experimenting with streaming processing engines (SPEs) and incremental machine learning (IML) models. The main features of Sail are:
 
@@ -27,7 +28,7 @@ Sail could have been parallelized using Spark as well. However, to keep the stre
 
 # 🛠 Installation
 
-Sail is intended to work with **Python 3.10 and above**. You can install the latest version from GitHub as so:
+Sail is intended to work with **Python 3.7 and above**. You can install the latest version from GitHub as so:
 
 ```sh
 git clone https://github.com/IBM/sail.git
--- src/sail/models/keras/wglstm.py
+++ src/sail/models/keras/wglstm.py
@@ -4,26 +4,22 @@ Weighted Gradient learning based LSTM (WGLSTM) neural network
 Code adapted from https://github.com/weilai0980/onlineLearning
 """
 
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.layers import (
-    Dense,
-    Activation,
-    LSTM,
-    TimeDistributed,
-)
 import copy
-import scipy.stats as st
-import numpy as np
-from numpy.lib.scimath import sqrt
 from types import SimpleNamespace
-from sail.visualisation.ts_plot import plot_series
+
+import numpy as np
+import scipy.stats as st
+import tensorflow as tf
+from numpy.lib.scimath import sqrt
 from scikeras.wrappers import KerasRegressor
+from tensorflow.keras.layers import LSTM, Activation, Dense, TimeDistributed
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.regularizers import l2
+
+from sail.models.keras.base import KerasSerializationMixin
 from sail.utils.stats import nmse
 from sail.visualisation.ts_plot import plot_series
-from sail.models.keras.base import KerasSerializationMixin
 
 
 class _Model(Sequential):
@@ -51,7 +47,6 @@ class _Model(Sequential):
         self.add(Activation(hidden_layer_activation))
 
     def fit(self, x, y, **kwargs):
-
         """
         Trains the model for a fixed number of epochs
         (iterations on a dataset).
@@ -95,9 +90,11 @@ class _Model(Sequential):
                 cur_pred_ini = cur_prediction[0][0][0]
                 tmpresi = tmp_cur_trny[0][0][0] - cur_pred_ini
 
-                norm_diff, susp_diff, point_diff = self.sliding_window_features(
-                    i + 1, x, y, win_susp
-                )
+                (
+                    norm_diff,
+                    susp_diff,
+                    point_diff,
+                ) = self.sliding_window_features(i + 1, x, y, win_susp)
 
                 tmpw = self.sliding_window_weight(
                     norm_diff,
@@ -114,7 +111,10 @@ class _Model(Sequential):
                 tmp_zval = (tmpresi - curmean) * 1.0 / sqrt(curvar)
                 tmp_pro_conve = st.norm.cdf(tmp_zval)
 
-                if tmp_pro_conve > (1 - conf_level) or tmp_pro_conve < conf_level:
+                if (
+                    tmp_pro_conve > (1 - conf_level)
+                    or tmp_pro_conve < conf_level
+                ):
                     sus_point_bool = True
                     pre_susp_bool = True
                     weight = tmpw
@@ -174,9 +174,9 @@ class _Model(Sequential):
             resi_mean = resi_mean * seg_cnt * 1.0 / (seg_cnt + 1) + tmpresi / (
                 seg_cnt + 1
             )
-            resi_sqr = resi_sqr * seg_cnt * 1.0 / (seg_cnt + 1) + tmpresi * tmpresi / (
+            resi_sqr = resi_sqr * seg_cnt * 1.0 / (
                 seg_cnt + 1
-            )
+            ) + tmpresi * tmpresi / (seg_cnt + 1)
             resi_var = resi_sqr - resi_mean * resi_mean
             seg_cnt = seg_cnt + 1
 
@@ -201,7 +201,9 @@ class _Model(Sequential):
         #     save_path="plot_wglstm.png",
         # )
 
-        return SimpleNamespace(history={m.name: m.result() for m in self.metrics})
+        return SimpleNamespace(
+            history={m.name: m.result() for m in self.metrics}
+        )
 
     def sliding_window_features(self, cur_pos, dataX, dataY, susp_list):
         winsize = self.window_size
@@ -228,13 +230,18 @@ class _Model(Sequential):
                     tmpdiff.append(0.0)
                 else:
                     tmpdiff.append(
-                        abs(cur_val - dataX[:, lnormal : lnormal + 1, :][0][0][0])
+                        abs(
+                            cur_val
+                            - dataX[:, lnormal : lnormal + 1, :][0][0][0]
+                        )
                     )
                     lnormal = i
             else:
                 rsus = i
                 if i >= (cur_pos - winsize):
-                    tmpdiff.append(abs(cur_val - dataX[:, i - 1 : i, :][0][0][0]))
+                    tmpdiff.append(
+                        abs(cur_val - dataX[:, i - 1 : i, :][0][0][0])
+                    )
                 else:
                     tmpdiff.append(abs(0.0))
 
@@ -247,13 +254,19 @@ class _Model(Sequential):
                     rnormal = tmp_dta
                 else:
                     tmpdiff[tmp_win] = tmpdiff[tmp_win] + (
-                        abs(cur_val - dataX[:, rnormal : rnormal + 1, :][0][0][0])
+                        abs(
+                            cur_val
+                            - dataX[:, rnormal : rnormal + 1, :][0][0][0]
+                        )
                     )
                     rnormal = tmp_dta
             else:
                 if tmp_dta <= (cur_pos - 1):
                     tmpdiff[tmp_win] = tmpdiff[tmp_win] + (
-                        abs(cur_val - dataX[:, tmp_dta + 1 : tmp_dta + 2, :][0][0][0])
+                        abs(
+                            cur_val
+                            - dataX[:, tmp_dta + 1 : tmp_dta + 2, :][0][0][0]
+                        )
                     )
 
         tmparr = list(zip(tmpdiff, susp_list))
@@ -279,7 +292,13 @@ class _Model(Sequential):
         weight_beta = 0.05
 
         if len(suspDiff) != 0 and len(normDiff) != 0:
-            r2 = 1.0 * sum(suspDiff) * len(normDiff) / sum(normDiff) / len(suspDiff)
+            r2 = (
+                1.0
+                * sum(suspDiff)
+                * len(normDiff)
+                / sum(normDiff)
+                / len(suspDiff)
+            )
             if r2 < 5:
                 r2 = 0.0
 
@@ -357,7 +376,9 @@ class WGLSTM(KerasRegressor, KerasSerializationMixin):
     def __init__(
         self,
         loss="mse",
-        optimizer=SGD(lr=0.002, momentum=0.03, decay=0.0, nesterov=True),
+        optimizer=SGD(
+            learning_rate=0.002, momentum=0.03, weight_decay=0.0, nesterov=True
+        ),
         metrics=None,
         epochs=1,
         verbose=0,
@@ -369,7 +390,6 @@ class WGLSTM(KerasRegressor, KerasSerializationMixin):
         window_size=20,
         **kwargs,
     ) -> None:
-
         super(WGLSTM, self).__init__(
             _Model(
                 num_of_features,

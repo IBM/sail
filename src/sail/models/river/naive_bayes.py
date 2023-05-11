be9a0eb1276caae5fcf6a44a81c9366e2b450f94
Dhaval Salwala <Dhaval.Vinodbhai.Salwala@ibm.com>
2023-05-11 23:15:42 +0100

    River API changes

--- src/sail/models/river/naive_bayes.py
+++ src/sail/models/river/naive_bayes.py
@@ -1,29 +1,30 @@
 import river.naive_bayes as naive_bayes
-from river.compat import River2SKLClassifier
+from sail.models.river.base import SailRiverClassifier
+
 
 __all__ = ["GaussianNB", "MultinomialNB", "ComplementNB", "BernoulliNB"]
 
 
-class GaussianNB(River2SKLClassifier):
+class GaussianNB(SailRiverClassifier):
     def __init__(self):
         super(GaussianNB, self).__init__(river_estimator=naive_bayes.GaussianNB())
 
 
-class MultinomialNB(River2SKLClassifier):
+class MultinomialNB(SailRiverClassifier):
     def __init__(self, alpha=1.0):
         super(MultinomialNB, self).__init__(
             river_estimator=naive_bayes.MultinomialNB(alpha)
         )
 
 
-class ComplementNB(River2SKLClassifier):
+class ComplementNB(SailRiverClassifier):
     def __init__(self, alpha=1.0):
         super(ComplementNB, self).__init__(
             river_estimator=naive_bayes.ComplementNB(alpha)
         )
 
 
-class BernoulliNB(River2SKLClassifier):
+class BernoulliNB(SailRiverClassifier):
     def __init__(self, alpha=1.0, true_threshold=0.0):
         super(BernoulliNB, self).__init__(
             river_estimator=naive_bayes.BernoulliNB(alpha, true_threshold)

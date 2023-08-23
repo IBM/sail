a73acd00b02213b198460f0cfdc14802b0d6eab1
Dhaval Salwala <dhaval.vinodbhai.salwala@ibm.com>
2023-08-23 22:36:06 +0100

    river base file fix.
    Signed-off-by: Dhaval Salwala <dhaval.vinodbhai.salwala@ibm.com>

--- src/sail/models/river/base.py
+++ src/sail/models/river/base.py
@@ -1,12 +1,29 @@
-from river.compat import River2SKLClassifier, River2SKLRegressor
-from sail.utils.mixin import RiverAttributeMixin
+import inspect
+import typing
+
+from river.compat import (
+    River2SKLClassifier,
+    River2SKLRegressor,
+)
+
 from sail.models.base import SAILModel
 from sail.utils.logging import configure_logger
 
 LOGGER = configure_logger(logger_name="River")
 
 
-class RiverBase(SAILModel, RiverAttributeMixin):
+class RiverMixin:
+    def __getattr__(self, __name: str) -> typing.Any:
+        return getattr(self.river_estimator, __name)
+
+    def __setattr__(self, __name: str, __value: typing.Any) -> None:
+        if __name in list(inspect.signature(__class__).parameters):
+            setattr(self.river_estimator, __name, __value)
+        else:
+            super().__setattr__(__name, __value)
+
+
+class RiverBase(SAILModel, RiverMixin):
     pass
 
 
